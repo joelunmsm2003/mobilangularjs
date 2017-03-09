4f8e3e5fc7083c03f2c23ddf00fd1f761112db25
xiencias01 <xiencias01@xiencias01.xiencias.com>
2017-03-09 15:29:48 -0500

    Agrego servicios de BBVA

---
+++ services/bbva.js
@@ -0,0 +1,111 @@
+function TipificaService ($http,$q,$log,$localStorage) {  
+
+
+    return {
+        buscardni:buscardni,
+        actualizar:actualizar,
+        ventas:ventas,
+        actualizarchubb:actualizarchubb
+
+    }
+
+        function buscardni(dni){
+
+
+        var defered = $q.defer();
+        var promise = defered.promise;
+
+        $http({
+
+        url: host+"buscardni",
+        data: data,
+        method: 'POST'
+        }).
+        success(function(data) {
+
+
+        return promise;
+
+        })
+
+    }
+
+
+
+
+
+
+
+        function actualizar(dni){
+
+
+        var defered = $q.defer();
+        var promise = defered.promise;
+
+        $http({
+
+        url: host+"actualizar",
+        data: data,
+        method: 'POST'
+        }).
+        success(function(data) {
+
+
+        return promise;
+
+        })
+
+    }
+
+
+
+
+
+
+        function ventas(dni){
+
+
+        var defered = $q.defer();
+        var promise = defered.promise;
+
+        $http({
+
+        url: host+"ventas",
+        data: data,
+        method: 'POST'
+        }).
+        success(function(data) {
+
+
+        return promise;
+
+        })
+
+    }
+
+
+
+
+
+
+
+        function actualizarchubb(dni){
+
+
+        var defered = $q.defer();
+        var promise = defered.promise;
+
+        $http({
+
+        url: host+"actualizarchubb",
+        data: data,
+        method: 'POST'
+        }).
+        success(function(data) {
+
+
+        return promise;
+
+        })
+
+    }
