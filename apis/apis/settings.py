e79a257e7ffe400938c0fa6832bab475d85fdb64
joel <joelunmsm@gmail.com>
2017-03-24 15:31:59 -0500

    trama casi finalizada

--- apis/apis/settings.py
+++ apis/apis/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'orionc7',
         'USER': 'root',
         'PASSWORD': '123',
-        'HOST': '127.0.0.1', 
+        'HOST': '192.168.40.231', 
         'PORT': '3306'
      
     }
