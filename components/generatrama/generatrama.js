a64a6e6e75dab70cc38e3fa3ccabca625a3d5529
root <root@xiencias01.xiencias.com>
2017-03-24 13:49:42 -0500

    trama

--- components/generatrama/generatrama.js
+++ components/generatrama/generatrama.js
@@ -18,20 +18,34 @@ function GeneratramaController($scope,$location,$http,BbvaService){
 
 	$scope.agregar=function(data){
 
-		 BbvaService.trama(data).then(function(data) {
-
-
-        })
 
 		 BbvaService.generatrama(data).then(function(data) {
 
             console.log('Datos del trama',data)
+
+            $scope.base=data
 
         })
 
 
 
 	 }
+
+
+
+  $scope.genera_trama=function(data){
+
+
+     BbvaService.trama(data).then(function(data) {
+
+      console.log(data)
+
+
+        })
+
+
+
+   }
 
 
 
