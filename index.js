fb40d4a0e4311b5a4004ffb921e3d2baf77d1473
joel <joelunmsm@gmail.com>
2017-03-27 13:24:43 -0500

    prieba

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ angular
 
     host='http://192.168.40.231:8000/'
 
-    console.log('Esto no deberia nnn')
+    console.log('Esto no deberiassss nnn')
 
     host_primary='http://192.168.40.4/'
     
